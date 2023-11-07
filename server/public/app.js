69705b4d5f422b1261278c3dda51e2d9df45c90f
xtarzyk <61456484+xtarzyk@users.noreply.github.com>
2023-11-07 19:30:08 +0100

    added new look and client side features

--- server/public/app.js
+++ server/public/app.js
@@ -1,23 +1,58 @@
 const socket = io("ws://localhost:3500");
 
+const msgInput = document.querySelector("#message");
+const nameInput = document.querySelector("#name");
+const chatRoom = document.querySelector("#room");
+const activity = document.querySelector(".activity");
+const userList = document.querySelector(".user-list");
+const roomList = document.querySelector(".room-list");
+const chatDisplay = document.querySelector(".chat-display");
+
 function sendMessage(e) {
   e.preventDefault();
-
-  const input = document.querySelector("input");
-
-  if (input.value) {
-    socket.emit("message", input.value);
-    input.value = "";
+  if (nameInput.value && msgInput.value && chatRoom.value) {
+    socket.emit("message", {
+      name: nameInput.value,
+      text: msgInput.value,
+    });
+    msgInput.value = "";
   }
-
-  input.focus();
+  msgInput.focus();
 }
 
-document.querySelector("form").addEventListener("submit", sendMessage);
+function enterRoom(e) {
+  e.preventDefault();
+  if (nameInput.value && chatRoom.value) {
+    socket.emit("enterRoom", {
+      name: nameInput.value,
+      room: chatRoom.value,
+    });
+  }
+}
 
+document.querySelector("form-msg").addEventListener("submit", sendMessage);
+
+document.querySelector("form-join").addEventListener("submit", enterRoom);
+
+msgInput.addEventListener("keypress", () => {
+  socket.emit("activity", nameInput.value);
+});
+
+// Listen for messages
 socket.on("message", (data) => {
+  activity.textContent = "";
   const li = document.createElement("li");
-
   li.textContent = data;
   document.querySelector("ul").appendChild(li);
+});
+
+let activityTimer;
+socket.on("activity", (name) => {
+  activity.textContent = `${name} is typing...`;
+
+  // Clear after 3 seconds
+  clearTimeout(activityTimer);
+  activityTimer = setTimeout(() => {
+    activity.textContent = "";
+  }, 1000);
 });
